23878f5d424afb9aa34dd5f2a6e8d912ea05e38e
GDMane <47275172+GDMane@users.noreply.github.com>
2019-09-28 17:08:42 +0530

    Santu bhau laptop
    Tnyacha ashirvad

---
+++ firstTest.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+myDriver = webdriver.Chrome("E://Ganesh//Driver//chromedriver.exe")
+
+myDriver.get("https://www.google.com")
+myDriver.maximize_window()
+myEdit = myDriver.find_element_by_name("q")
+myEdit.send_keys("Queue Codes Technology")
+
+myButton = myDriver.find_element_by_name("btnK")
+myButton.click()
+
