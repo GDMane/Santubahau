886223778d727db9f87fb7cbb3a8f4fa03158d90
GDMane <2611ganesh@gmail.com>
2019-10-13 08:16:56 +0530

    New commit from Sagar's laptop :)

--- firstTest.py
+++ firstTest.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-myDriver = webdriver.Chrome("E://GMtes//Santubhau//Drivers//chromedriver.exe")
+myDriver = webdriver.Chrome("D://GMtes//Santubhau//Drivers//chromedriver.exe")
 
 myDriver.get("https://www.google.com")
 myDriver.maximize_window()
 myEdit = myDriver.find_element_by_name("q")
 myEdit.send_keys("Queue Codes Technology")
 
-#myButton = myDriver.find_element_by_name("btnK")
+myButton = myDriver.find_element_by_name("btnK")
 myEdit.send_keys(Keys.ENTER)
 
-print ("GM Push Test")
+print("GM Push Test")
